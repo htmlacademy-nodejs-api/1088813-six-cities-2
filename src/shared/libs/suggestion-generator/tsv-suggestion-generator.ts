588c098780df08ac929675c33b8a422cc6c8c46c
Sergey Kuzmichev <drumer19940310@mail.ru>
2025-07-23 16:34:42 +0300

    Добавлено: - сгруппировал константы для генератора в отдельный Enum

--- src/shared/libs/suggestion-generator/tsv-suggestion-generator.ts
+++ src/shared/libs/suggestion-generator/tsv-suggestion-generator.ts
@@ -2,23 +2,9 @@ import {SuggestionGenerator} from './suggestion-generator.interface.js';
 import {MockServerData} from '../../types/index.js';
 import {generateRandomNumber, getRandomItem, getRandomItems} from '../../helpers/index.js';
 import dayjs from 'dayjs';
-
-const MIN_PRICE = 100;
-const MAX_PRICE = 100000;
-
-const MIN_ROOMS = 1;
-const MAX_ROOMS = 8;
-
-const MIN_GUESTS = 1;
-const MAX_GUESTS = 8;
-
-const MIN_RATING = 1;
-const MAX_RATING = 5;
+import {SuggestionGeneratorConstants} from '../../enums/suggestion-generator.js';
 
 const FLAGS = [true, false];
-
-const FIRST_WEEK_DAY = 1;
-const LAST_WEEK_DAY = 7;
 
 export class TSVSuggestionGenerator implements SuggestionGenerator {
   constructor(private readonly mockData: MockServerData) {
@@ -33,10 +19,10 @@ export class TSVSuggestionGenerator implements SuggestionGenerator {
     const imagesGallery = getRandomItems<string>(this.mockData.imagesGallery).join(';');
     const premium = getRandomItem<boolean>(FLAGS);
     const favourite = getRandomItem<boolean>(FLAGS);
-    const rating = generateRandomNumber(MIN_RATING, MAX_RATING);
-    const roomsCount = generateRandomNumber(MIN_ROOMS, MAX_ROOMS);
-    const guestsCount = generateRandomNumber(MIN_GUESTS, MAX_GUESTS);
-    const rentPrice = generateRandomNumber(MIN_PRICE, MAX_PRICE);
+    const rating = generateRandomNumber(SuggestionGeneratorConstants.MIN_VALUE, SuggestionGeneratorConstants.MAX_RATING);
+    const roomsCount = generateRandomNumber(SuggestionGeneratorConstants.MIN_VALUE, SuggestionGeneratorConstants.MAX_ROOMS);
+    const guestsCount = generateRandomNumber(SuggestionGeneratorConstants.MIN_VALUE, SuggestionGeneratorConstants.MAX_GUESTS);
+    const rentPrice = generateRandomNumber(SuggestionGeneratorConstants.MIN_PRICE, SuggestionGeneratorConstants.MAX_PRICE);
     const conveniences = getRandomItems<string>(this.mockData.conveniences).join(';');
     const author = getRandomItem<string>(this.mockData.users);
     const email = getRandomItem<string>(this.mockData.emails);
@@ -44,7 +30,7 @@ export class TSVSuggestionGenerator implements SuggestionGenerator {
     const commentsCount = generateRandomNumber(1, 500);
 
     const createdDate = dayjs()
-      .subtract(generateRandomNumber(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day')
+      .subtract(generateRandomNumber(SuggestionGeneratorConstants.MIN_VALUE, SuggestionGeneratorConstants.LAST_WEEK_DAY), 'day')
       .toISOString();
 
     const [firstName, lastName] = author.split(' ');
